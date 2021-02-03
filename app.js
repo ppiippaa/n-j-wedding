69c182328ed97c855de54a8782a63603d467c55e
Pippa <pip.thompson96@gmail.com>
2021-02-03 21:22:58 +0000

    add picture caption/hover feature

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+// Smooth scroll functionality
 document.querySelectorAll('a[href^="#"]').forEach(anchor => {
     anchor.addEventListener('click', function (e) {
         e.preventDefault();
@@ -7,3 +8,22 @@ document.querySelectorAll('a[href^="#"]').forEach(anchor => {
         });
     });
 });
+
+// Hover effect on story images
+
+for (let i = 0; i < document.querySelectorAll('.image').length; i++) {
+
+    document.querySelectorAll('.image')[i].addEventListener('mouseenter', function() {
+        this.nextElementSibling.removeAttribute('hidden');
+        console.log(this.nextElementSibling);
+    })
+}
+
+for (let i = 0; i < document.querySelectorAll('.image').length; i++) {
+
+    document.querySelectorAll('.image')[i].addEventListener('mouseout', function () {
+        this.nextElementSibling.setAttribute('hidden', true);
+        console.log(this);
+    })
+}
+
